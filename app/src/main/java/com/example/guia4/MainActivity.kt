da1e2e1692287724ecba620d34c778e04d0a4d93
Brian <brian.almendras@ucb.edu.bo>
2023-03-02 22:55:47 -0400

    Add Guide 5 "Guia Menus Icons"

--- app/src/main/java/com/example/guia4/MainActivity.kt
+++ app/src/main/java/com/example/guia4/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.guia4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +17,20 @@ class MainActivity : AppCompatActivity() {
             setSupportActionBar(toolBar)
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.menu_item1->{
+                Log.d("TEST","PRESS ICON MENU")
+                true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
