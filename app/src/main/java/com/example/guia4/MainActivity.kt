aea3d9351ea9989be8b2473f57a2e1abf34aae84
Brian <brian.almendras@ucb.edu.bo>
2023-03-02 23:08:55 -0400

    Add Guide 3 "Cambiar los colores de la pantalla con botones"

--- app/src/main/java/com/example/guia4/MainActivity.kt
+++ app/src/main/java/com/example/guia4/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.guia4
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toolbar
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
+    private val btnWhite: Button
+        get()=findViewById(R.id.btn_white)
+    private val btnRed: Button
+        get()=findViewById(R.id.btn_red)
+    private val layoutPrincipal: ConstraintLayout
+        get()=findViewById(R.id.layoutPrincipal)
     private val toolBar: androidx.appcompat.widget.Toolbar
         get()=findViewById(R.id.toolbar)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +24,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar.let {
             setSupportActionBar(toolBar)
+        }
+        btnRed.setOnClickListener {
+            layoutPrincipal.setBackgroundColor(Color.RED)
+        }
+        btnWhite.setOnClickListener {
+            layoutPrincipal.setBackgroundColor(Color.WHITE)
         }
 
     }
