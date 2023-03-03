1f3c4572874aaae63dbdd0802b76eef48cf47bf9
Brian <brian.almendras@ucb.edu.bo>
2023-03-02 23:36:17 -0400

    Add Guide 6 "Guia SnackBar"

--- app/src/main/java/com/example/guia4/MainActivity.kt
+++ app/src/main/java/com/example/guia4/MainActivity.kt
@@ -9,6 +9,7 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private val btnWhite: Button
@@ -19,6 +20,8 @@ class MainActivity : AppCompatActivity() {
         get()=findViewById(R.id.layoutPrincipal)
     private val toolBar: androidx.appcompat.widget.Toolbar
         get()=findViewById(R.id.toolbar)
+    private val btnSnackbar: Button
+        get() = findViewById(R.id.btnSnackbar)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,6 +33,12 @@ class MainActivity : AppCompatActivity() {
         }
         btnWhite.setOnClickListener {
             layoutPrincipal.setBackgroundColor(Color.WHITE)
+        }
+        btnSnackbar.setOnClickListener {
+            val mySnackbar=Snackbar.make(findViewById(R.id.layoutPrincipal),R.string.app_name,
+                Snackbar.LENGTH_LONG)
+            mySnackbar.setAction(R.string.undo_string, MyUndoListener())
+            mySnackbar.show()
         }
 
     }
---
+++ app/src/main/java/com/example/guia4/MyUndoListener.kt
@@ -0,0 +1,10 @@
+package com.example.guia4
+
+import android.util.Log
+import android.view.View
+
+class MyUndoListener: View.OnClickListener {
+    override fun onClick(v: View?) {
+        Log.d("TEST","UNDO PRESSED")
+    }
+}
