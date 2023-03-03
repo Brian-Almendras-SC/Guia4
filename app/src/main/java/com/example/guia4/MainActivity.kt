d67bfccf954f10fa65a76cd40833d26be5db4eee
Brian <brian.almendras@ucb.edu.bo>
2023-03-02 22:29:12 -0400

    Add action bar

--- app/src/main/java/com/example/guia4/MainActivity.kt
+++ app/src/main/java/com/example/guia4/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.guia4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+    private val toolBar: androidx.appcompat.widget.Toolbar
+        get()=findViewById(R.id.toolbar)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar.let {
+            setSupportActionBar(toolBar)
+        }
+
     }
 }
